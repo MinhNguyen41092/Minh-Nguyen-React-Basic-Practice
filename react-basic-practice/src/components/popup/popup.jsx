184fb17e6e91e5b2971d0b3dacd03ca9ca00af04
minhng271 <minhng271@gmail.com>
2022-12-08 16:24:27 +0700

    feat: #28 create popup

---
+++ react-basic-practice/src/components/popup/popup.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from '../common/Button';
+// alias path
+
+function Popup(props) {
+  const {
+    className, status, icon, message,
+  } = props;
+
+  return (
+    <div className="notification toast">
+      <Button
+        className="btn btn-close"
+        text="X"
+      />
+      <div className="notification-icon">
+        <img src={icon} alt="" />
+      </div>
+      <div>
+        <p className="notification-title">{status}</p>
+        <p className="notification-message">{message}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
