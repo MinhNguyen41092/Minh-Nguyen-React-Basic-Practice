a2502487e9d7f770775cc98843c9bb5d84574b43
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-12-13 11:46:28 +0700

    fix: #4 update button

--- react-basic-practice/src/components/common/button/index.jsx
+++ react-basic-practice/src/components/common/button/index.jsx
@@ -2,15 +2,16 @@ import './index.css';
 import React from 'react';
 
 const Button = (props) => {
-  const { onClick, className, text } = props;
+  const { onClick, className, text, icon } = props;
 
   return (
     <button
       type="button"
       onClick={onClick}
       className={className}
+      value="button"
     >
-      {text}
+      { text ? text : <img className="icon" src={icon} alt="icon" />}
     </button>
   );
 }
