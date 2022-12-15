410f1b76590b7a3ecc4ce91670841b97c88ffd79
minhng271 <minhng271@gmail.com>
2022-12-15 17:30:25 +0700

    feat: #10 create logo

---
+++ react-basic-practice/src/components/common/Logo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => (
+  <h1 className="logo">
+    <img className="logo" src="src/assets/images/Logo.png" alt="" />
+  </h1>
+);
+
+export default Logo;
