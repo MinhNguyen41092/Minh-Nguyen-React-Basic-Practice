5ad1792d1c44e7f26a867ba24fc5e4a240dc266d
minhng271 <minhng271@gmail.com>
2022-12-16 15:09:12 +0700

    fix: #10 update UI

--- react-basic-practice/src/page/SignUpPage/index.jsx
+++ react-basic-practice/src/page/SignUpPage/index.jsx
@@ -12,42 +12,10 @@ import './index.css';
 
 const SignUpPage = () => {
   const [isSignUpLoading, setIsSignUpLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState({});
   const inputUsername = useRef(null);
-  const inputEmail = useRef(null);
   const inputPassword = useRef(null);
-  // const navigate = useNavigate();
-
-  const handleSignUp = async (e) => {
-    try {
-      e.preventDefault();
-
-      setIsSignUpLoading(true);
-      const dataUser = await getUserByMail();
-      // Checking if the email is already exist.
-      const validate = dataUser.some((user) => user.email === inputUsername.current?.value);
-
-      // if username is already exist show error
-      if (validate) {
-        setErrorMessage('Username is already exist. Please try another one');
-      } else {
-      /* Creating a new user object and then sending it to the API to create a new user. */
-        const newUser = {
-          id: uuid(),
-          userName: inputUsername.current?.value || '',
-          email: inputEmail.current?.value || '',
-          password: inputPassword.current?.value || '',
-        };
-
-        await createUser(newUser);
-        navigate('/login');
-      }
-      setIsSignUpLoading(false);
-    } catch (error) {
-      alert(`Registration Fail. Please try again ${error}`);
-      setIsSignUpLoading(false);
-    }
-  };
+  const inputEmail = useRef(null);
 
   return (
     <>
@@ -67,8 +35,7 @@ const SignUpPage = () => {
             cssClasses="input-form input-email"
             placeholder="minhng@gmail.com"
             refer={inputEmail}
-            pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-            required="this is a required"
+            // pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
           />
 
           <Input
@@ -79,8 +46,7 @@ const SignUpPage = () => {
             placeholder="Minh Nguyen"
             message="Invalid username"
             refer={inputUsername}
-            pattern="^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$"
-            required="this is a required"
+            // pattern="^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$"
           />
 
           <Input
@@ -91,8 +57,7 @@ const SignUpPage = () => {
             placeholder="Password at least 6 characters"
             message="Invalid password"
             refer={inputPassword}
-            pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$"
-            required="this is a required"
+            // pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$"
           />
 
           {errorMessage && <span className="form-sign-up-error-message">{errorMessage}</span>}
