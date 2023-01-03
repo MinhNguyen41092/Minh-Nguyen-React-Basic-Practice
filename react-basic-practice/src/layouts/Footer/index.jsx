ef32bdc7b76e1635acbbeccace228133d614d883
minhng271 <minhng271@gmail.com>
2023-01-03 16:27:54 +0700

    fix: #15 update style for footer

--- react-basic-practice/src/layouts/Footer/index.jsx
+++ react-basic-practice/src/layouts/Footer/index.jsx
@@ -1,51 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navigation from '../../components/Navigation';
+// import Navigation from '../../components/Navigation';
+import Navigation from '@/components/Navigation';
 import Input from '../../components/common/Input';
 import Button from '../../components/common/Button';
 
+import './index.css';
+
 const Footer = () => (
-  <Footer>
+  <footer className="footer-wrapper">
     <div className="footer-action">
       <Navigation />
-      <p>
+      <p className="footer-introduction">
         <span>© 2021 Shelly</span>
         . Terms of use
-        <span>and</span>
+        <span> and </span>
         privacy policy.
       </p>
     </div>
     <div className="footer-social">
       <div className="footer-social email">
-        <Input />
-        <Button />
+        <Input
+          className="input-email"
+          placeholder="Give an email, get the newsletter."
+        />
+        <Button
+          className="btn-arrow"
+          type="button"
+          icon="./src/assets/images/iconButton/btn-arrow.png"
+        />
       </div>
       <div className="footer-social icon">
-        <ul>
-          <li>
+        <ul className="footer-social list">
+          <li className="footer-social item">
             <Link href="/">
-              <img src="" alt="" />
+              <img src="./src/assets/images/socialLogo/linkedin-logo.jpg" alt="" />
             </Link>
           </li>
-          <li>
+          <li className="footer-social item">
             <Link href="/">
-              <img src="" alt="" />
+              <img src="./src/assets/images/socialLogo/facbook-logo.png" alt="" />
             </Link>
           </li>
-          <li>
+          <li className="footer-social item">
             <Link href="/">
-              <img src="" alt="" />
+              <img src="./src/assets/images/socialLogo/instagram-logo.png" alt="" />
             </Link>
           </li>
-          <li>
+          <li className="footer-social item">
             <Link href="/">
-              <img src="" alt="" />
+              <img src="./src/assets/images/socialLogo/twitter-logo.png" alt="" />
             </Link>
           </li>
         </ul>
       </div>
     </div>
-  </Footer>
+  </footer>
 );
 
 export default Footer;
