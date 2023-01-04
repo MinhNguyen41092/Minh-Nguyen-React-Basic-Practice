8d24ba93fa488fab6573b9cf5f234fc1baf24a93
minhng271 <minhng271@gmail.com>
2023-01-04 15:14:53 +0700

    feat: #44 create grid layout

---
+++ react-basic-practice/src/layouts/Grid/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+
+const Grid = (props) => {
+  const {
+    columns, columnGap, rowGap, children,
+  } = props;
+  const className = `
+  grid-container 
+  grid-columns-${columns} 
+  grid-columns-gap-${columnGap} 
+  grid-rows-gap-${rowGap}
+  `;
+
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default Grid;
