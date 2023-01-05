f3846823eccab071f7191b1f60a24fbd2bbafaa0
minhng271 <minhng271@gmail.com>
2023-01-05 14:36:19 +0700

    fix: #48 update loading data

--- react-basic-practice/src/App.jsx
+++ react-basic-practice/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ListProducts from './components/ListProducts';
-// import { LoadingProvider } from './contexts/loading';
+import { LoadingProvider } from './contexts/loading';
 // import Routing from './Routing';
 
 const App = () => (
   <div className="App">
     <Router>
-      {/* <LoadingProvider> */}
-      {/* <Routing /> */}
-      {/* </LoadingProvider> */}
-      <ListProducts />
+      <LoadingProvider>
+        {/* <Routing /> */}
+        <ListProducts />
+      </LoadingProvider>
     </Router>
   </div>
 );
--- react-basic-practice/src/components/ListProducts/index.jsx
+++ react-basic-practice/src/components/ListProducts/index.jsx
@@ -1,34 +1,47 @@
 import React, { useEffect, useState } from 'react';
+
 import Grid from '@/layouts/Grid';
 import ProductCard from '../ProductCard';
+
 import { getListProductsLimit } from '../../services/Products';
+import { useLoading } from '@/contexts/loading';
 
 const ListProducts = () => {
   const [products, setProducts] = useState([]);
+  const { loading, setLoading } = useLoading();
 
   useEffect(() => {
     const getData = async () => {
       const data = await getListProductsLimit(1);
       setProducts(data);
     };
-    getData();
+    try {
+      setLoading(true);
+      getData();
+    } catch {
+      alert('Error loading data, please reload the page');
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   return (
     <div className="list-products">
-      {products.length ? (
-        <Grid columns="3" rowGap="large" columnGap="large">
-          {products.map((item) => (
-            <ProductCard
-              product={item}
-            />
-          ))}
-        </Grid>
-      ) : (
-        <span className="error-no-posts">
-          There are no product to show right now
-        </span>
-      )}
+      {
+        loading
+          ? (
+            <p className="loading">Loading...</p>
+          )
+          : (
+            <Grid columns="3" rowGap="large" columnGap="large">
+              {products.map((item) => (
+                <ProductCard
+                  product={item}
+                />
+              ))}
+            </Grid>
+          )
+      }
     </div>
   );
 };
