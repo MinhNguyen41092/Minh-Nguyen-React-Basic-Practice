c35d0d52f61f9daf5da7a2e6891a7fa29282939e
minhng271 <49194444+minhng271@users.noreply.github.com>
2023-01-08 18:03:59 +0700

    fix: #50 fix comments

--- react-basic-practice/src/pages/DetailProductPage/index.jsx
+++ react-basic-practice/src/pages/DetailProductPage/index.jsx
@@ -8,44 +8,10 @@ import { useLoading } from '@/contexts/loading';
 
 import './index.css';
 
-const DetailProductPage = (props) => {
+const ProductDetail = (props) => {
   const { id } = props;
   const { loading, setLoading } = useLoading();
   const [product, setProduct] = useState([]);
-
-  const handleAddCart = () => {};
-
-  const renderProduct = () => (
-    product.map((field) => (
-      <div className="product-main">
-        <div className="product">
-          <img className="product-image" src={field.image} alt="" />
-          <div className="product-information">
-            <span className="product-detail name">{field.name}</span>
-            <span className="product-detail price">{`$ ${field.price}`}</span>
-            <p className="product-detail description">{field.description}</p>
-            <div className="add-cart">
-              <Quantity />
-              <Button
-                type="button"
-                onClick={handleAddCart}
-                className="btn-primary btn-large"
-                text="add to cart"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="product-description">
-          <div className="product-detail title">
-            Description
-          </div>
-          <div className="product-detail description">
-            {field.description}
-          </div>
-        </div>
-      </div>
-    ))
-  );
 
   useEffect(() => {
     const getData = async () => {
@@ -63,6 +29,8 @@ const DetailProductPage = (props) => {
     getData();
   }, []);
 
+  const handleAddCart = () => {};
+
   return (
     <DefaultLayout>
       {
@@ -73,7 +41,35 @@ const DetailProductPage = (props) => {
           : (
             <>
               {
-                renderProduct()
+                product.map((field) => (
+                  <div className="product-main">
+                    <div className="product">
+                      <img className="product-image" src={field.image} alt="" />
+                      <div className="product-information">
+                        <span className="product-detail name">{field.name}</span>
+                        <span className="product-detail price">{`$ ${field.price}`}</span>
+                        <p className="product-detail description">{field.description}</p>
+                        <div className="add-cart">
+                          <Quantity />
+                          <Button
+                            type="button"
+                            onClick={handleAddCart}
+                            className="btn-primary btn-large"
+                            text="add to cart"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="product-description">
+                      <div className="product-detail title">
+                        Description
+                      </div>
+                      <div className="product-detail description">
+                        {field.description}
+                      </div>
+                    </div>
+                  </div>
+                ))
               }
             </>
           )
@@ -82,4 +78,4 @@ const DetailProductPage = (props) => {
   );
 };
 
-export default DetailProductPage;
+export default ProductDetail;
