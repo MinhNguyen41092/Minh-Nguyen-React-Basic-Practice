02ddd4fe07f7dcc75eee92a0393d720e33bbad11
minhng271 <minhng271@gmail.com>
2023-01-13 14:38:37 +0700

    feat: #25 style for cartbar

--- react-basic-practice/src/components/CartItem/index.jsx
+++ react-basic-practice/src/components/CartItem/index.jsx
@@ -4,14 +4,14 @@ import closeButton from '@/assets/images/iconButton/btn-close.png';
 import './index.css';
 
 const CartItem = (props) => {
-  const { item, onHandleDeleteCart } = props;
+  const { item, deleteCartItem } = props;
   return (
     <article className="cart-item">
       <h4 className="name">{item.name}</h4>
       <p className="quantity">{`Quantity: ${item.quantity}`}</p>
       <p className="price">{`$ ${item.price}`}</p>
       <Button
-        onClick={onHandleDeleteCart}
+        onClick={deleteCartItem}
         type="button"
         className="btn btn-delete"
         icon={closeButton}
--- react-basic-practice/src/contexts/CartProvider.jsx
+++ react-basic-practice/src/contexts/CartProvider.jsx
@@ -1,18 +1,16 @@
 import React, { useContext, useMemo, useState } from 'react';
-import { updateCart } from '@services/index'
 
 const CartContext = React.createContext();
 const useCart = () => useContext(CartContext);
 const initCartList = [];
 
-
 const CartProvider = (props) => {
   const { children } = props;
   const [cartItem, setCartItem] = useState();
 
-  const addCart = useCallback()
-
+  const addCart = '';
   const deleteCart = '';
+
   const valueContext = useMemo(() => ({
     listCart: cartItem?.products || [],
     addCart,
@@ -26,6 +24,6 @@ const CartProvider = (props) => {
       {children}
     </CartContext.Provider>
   );
-}
+};
 
 export { useCart, CartProvider, initCartList };
