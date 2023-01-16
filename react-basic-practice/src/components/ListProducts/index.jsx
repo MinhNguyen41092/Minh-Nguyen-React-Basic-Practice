db68ec1b5f315c25f9a7ed8fd23df919edd9a9a7
minhng271 <minhng271@gmail.com>
2023-01-16 15:17:19 +0700

    feat: #20 create button load more

--- react-basic-practice/src/components/ListProducts/index.jsx
+++ react-basic-practice/src/components/ListProducts/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Grid from '@/layouts/Grid';
 import ProductCard from '../ProductCard';
+import Button from '../common/Button';
 
 import { getListProducts } from '../../services/Products';
 import { useLoading } from '@/contexts/LoadingProvider';
@@ -13,12 +14,13 @@ const ListProducts = (props) => {
   const { keyword } = props;
   const [products, setProducts] = useState([]);
   const { loading, setLoading } = useLoading();
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const getData = async () => {
       try {
         setLoading(true);
-        const data = await getListProducts(1, keyword);
+        const data = await getListProducts(pageNumber, keyword);
         data ? setProducts(data) : setProducts([]);
       } catch {
         alert('Error loading data, please reload the page');
@@ -28,7 +30,11 @@ const ListProducts = (props) => {
     };
 
     getData();
-  }, [keyword]);
+  }, [keyword, pageNumber]);
+
+  const handleLoadMore = () => {
+    setPageNumber(pageNumber + 1);
+  };
 
   return (
     <div className="list-products">
@@ -39,7 +45,7 @@ const ListProducts = (props) => {
           )
           : (
             <Grid columns="3" rowGap="large" columnGap="large">
-              {products.map((item) => (
+              {products && products.map((item) => (
                 <Link to={`/products/${item.id}`} key={item.id}>
                   <ProductCard product={item} />
                 </Link>
@@ -47,6 +53,12 @@ const ListProducts = (props) => {
             </Grid>
           )
       }
+      <Button
+        type="button"
+        onClick={handleLoadMore}
+        className="btn-primary btn-large"
+        text="load more"
+      />
     </div>
   );
 };
