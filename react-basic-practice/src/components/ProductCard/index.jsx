4a11af39030feda904d1d055494de2cb0da63c2f
minhng271 <49194444+minhng271@users.noreply.github.com>
2023-02-08 17:15:38 +0700

    fix: #82 fix function render label

--- react-basic-practice/src/components/ProductCard/index.jsx
+++ react-basic-practice/src/components/ProductCard/index.jsx
@@ -5,18 +5,21 @@ import './index.css';
 const ProductCard = (props) => {
   const { product } = props;
 
+  // Show label if the product is out of stock or discount
   const renderLabel = (quantity, discountPercent) => {
+    let label = '';
     if (quantity === 0) {
-      return <span className="product-label">Sold out</span>;
+      label = 'Sold out';
+    } else {
+      label = `${discountPercent} %`;
     }
-
-    return <span className="product-label">{`${discountPercent} %`}</span>;
+    return <span className="product-label">{label}</span>;
   };
 
   return (
     <article className={`product-card ${product.id}`}>
-      {(product.quantity <= 0 || product.discountPercent > 0)
-      && renderLabel(product.quantity, product.discountPercent)}
+      {(product.quantity <= 0 || product.discountPercent > 0) &&
+        renderLabel(product.quantity, product.discountPercent)}
       <Link to={`/products/${product.id}`} key={product.id}>
         <img className="img" src={product.image} alt="product" />
         <h4 className="name">{product.name}</h4>
